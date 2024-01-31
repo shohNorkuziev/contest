37ef7fc3788af7c39e309eb96248abe756883d09
student <student@vuz.magtu.ru>
2024-01-31 18:43:14 +0500

    add header footer

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  title = 'To-Do';
 
-  task={
-    id:1,
-    title:"shohruh",
-    isComplete: false
+  filter: "all" | "active" | "done" = "all"
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false },
+  ]
+
+  get item(){
+    if (this.filter === "all") {
+      return this.allItems
+    }
+    return this.allItems.filter((item) =>
+    this.filter === "done" ? item.done : !item.done
+    )
   }
+
 }
